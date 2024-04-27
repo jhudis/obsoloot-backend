0ade591045ddaa4b2324d2fcd797898b94cc7823
Justin Hudis <justin.hudis@gmail.com>
2024-04-27 19:09:48 +0000

    Add phone connection script

---
+++ connect_phones.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+adb -s ZY224G4625 tcpip 5555
+adb connect 192.168.1.184:5555
+sleep 4
+adb -s ZY22533K6V tcpip 5555
+adb connect 192.168.1.189:5555
+sleep 4
+adb -s ZY224G46FJ tcpip 5555
+adb connect 192.168.1.190:5555
+sleep 4
+adb -s ZY224XCX3Q tcpip 5555
+adb connect 192.168.1.191:5555
