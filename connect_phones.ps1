db4a8b7bea2163bfe3aece36a596a9f4220a6e0b
Justin Hudis <justin.hudis@gmail.com>
2024-04-27 15:20:45 -0400

    Convert phone connection bash script to PowerShell

--- connect_phones.ps1
+++ connect_phones.ps1
@@ -1,13 +1,11 @@
-#!/bin/bash
-
 adb -s ZY224G4625 tcpip 5555
 adb connect 192.168.1.184:5555
-sleep 4
+Start-Sleep -Seconds 10
 adb -s ZY22533K6V tcpip 5555
 adb connect 192.168.1.189:5555
-sleep 4
+Start-Sleep -Seconds 10
 adb -s ZY224G46FJ tcpip 5555
 adb connect 192.168.1.190:5555
-sleep 4
+Start-Sleep -Seconds 10
 adb -s ZY224XCX3Q tcpip 5555
 adb connect 192.168.1.191:5555
