6b65c2872ac200511c5a91a3335b7bad262434ca
Justin Hudis <justin.hudis@gmail.com>
2024-04-27 15:14:01 -0400

    Time provisioner script

--- provisioner.ts
+++ provisioner.ts
@@ -11,15 +11,23 @@ const code = `
     }
 `
 
+const N = 100;
+let completed = 0;
+
+console.time('find-primes');
 axios.post(`${HUB_URL}/upload?name=prime&method=isPrime`, code, { headers: { 'Content-Type': 'text/plain' } })
     .then(_res => {
-        for (let n = 0; n < 100; n++) {
+        for (let n = 0; n < N; n++) {
             axios.get(`${HUB_URL}/invoke?name=prime&args=${n}`)
                 .then(res => {
                     if (res.data == true) {
                         console.log(n);
                     } else if (res.data != false) {
                         console.log(res.data)
+                    }
+                    completed++;
+                    if (completed === N) {
+                        console.timeEnd('find-primes');
                     }
                 });
         }
